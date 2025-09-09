d8a3a146625116efbe3dfc6498204842c3bf2ac4
Andoree <and2900@inbox.ru>
2025-09-09 16:21:55 +0300

    Modified exception text

--- factowl/factowl/retrieval.py
+++ factowl/factowl/retrieval.py
@@ -300,7 +300,7 @@ class Retrieval(object):
             self.cache[cache_key] = self.get_gtr_passages(topic, retrieval_query, passages, k)
         if not (len(self.cache[cache_key]) in [k, len(passages)]):
             raise RuntimeError(f"Tried cache_key:{cache_key}, expected {k} or {len(passages)}.\n"
-                               f"> Passages: {len(self.cache[cache_key])} {passages}\n"
+                               f"> Passages: {len(passages)} {passages}\n"
                                f"> Cached passages: {len(self.cache[cache_key])} {self.cache[cache_key]}")
         # assert len(self.cache[cache_key]) in [k, len(passages)]
         self.add_n += 1
