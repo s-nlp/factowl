608f98612b8fec690dbc463eb223d727282829d6
Andoree <and2900@inbox.ru>
2026-03-17 23:39:11 +0300

    Import fix

--- factowl/clm_vllm.py
+++ factowl/clm_vllm.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from factowl.atomic_facts_sped_up_vllm import VLLMGenerator
+from factowl.atomic_facts import VLLMGenerator
 from vllm import SamplingParams
 from transformers import AutoTokenizer
 
